a077ab8ec0b9d0074ad70756d11b2ddb083f6e89
Chase Burnham <cburnhamiv@gmail.com>
2015-10-19 20:13:28 +0100

    Updated milli to time

--- app/src/main/java/letshangllc/timer/MilliToTime.java
+++ app/src/main/java/letshangllc/timer/MilliToTime.java
@@ -1,5 +1,7 @@
 package letshangllc.timer;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by cvburnha on 10/13/2015.
  */
@@ -10,9 +12,13 @@ public class MilliToTime {
 
     public int[] milliToTime(long milliseconds){
         int[] times = new int[4];
-        int seconds = (int) (milliseconds / 1000) % 60 ;
-        int minutes = (int) ((milliseconds / (1000*60)) % 60);
-        int hours   = (int) ((milliseconds / (1000*60*60)) % 24);
+
+        int hours = (int) milliseconds / (60 * 60 * 1000);
+        milliseconds -= hours * (60 * 60 * 1000);
+        int minutes = (int) milliseconds / (60 * 1000);
+        milliseconds -= minutes * (60 * 1000);
+        int seconds = (int) milliseconds / 1000;
+
         int milli= (int) (milliseconds % 1000);
         milli /=  10;
         times[0] = hours;
