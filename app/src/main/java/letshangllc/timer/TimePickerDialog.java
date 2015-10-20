09963d012dd2f7de3670576f5962b5bebffe6596
Chase Burnham <cburnhamiv@gmail.com>
2015-10-20 20:19:17 +0100

    Added functionallity to TimePickerDialog

--- app/src/main/java/letshangllc/timer/TimePickerDialog.java
+++ app/src/main/java/letshangllc/timer/TimePickerDialog.java
@@ -1,9 +1,38 @@
 package letshangllc.timer;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
 
 /**
  * Created by cvburnha on 10/20/2015.
  */
 public class TimePickerDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_number_picker, null))
+                // Add action buttons
+                .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        TimePickerDialog.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
 }
